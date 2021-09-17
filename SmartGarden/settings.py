46b007968fbaa44d7263418afc9439020562c550
Radovan Santa <84182964+radovansanta@users.noreply.github.com>
2021-09-17 09:17:16 +0200

    Update settings.py

--- SmartGarden/settings.py
+++ SmartGarden/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6x*d(pyv9qw7a4qk7_x*j35j9)x_#1ajhmu=o&d(#o*9=g0m)y'
+SECRET_KEY = 'secretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
